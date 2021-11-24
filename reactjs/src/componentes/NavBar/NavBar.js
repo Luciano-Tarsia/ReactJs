711b68fb0ad841960a822f5584e3a053f3d267f3
Luciano-Tarsia <lucianoftarsia@gmail.com>
2021-11-24 17:38:42 -0300

    menu e-commerce

---
+++ reactjs/src/componentes/NavBar/NavBar.js
@@ -0,0 +1,16 @@
+import "./NavBar.css"
+
+export const NavBar = () => {
+    return (
+        <header className= "navbar">
+            <h1>Coder RJ</h1>
+            <nav>
+                <ul>
+                    <li>Enlace</li>
+                    <li>Enlace</li>
+                    <li>Enlace</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
