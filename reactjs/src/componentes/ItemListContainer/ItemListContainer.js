113ea84e1d2711d9a6afda0c6ff5ec5999db0535
Luciano-Tarsia <lucianoftarsia@gmail.com>
2021-12-01 01:48:14 -0300

    Clase 5 - 2

--- reactjs/src/componentes/ItemListContainer/ItemListContainer.js
+++ reactjs/src/componentes/ItemListContainer/ItemListContainer.js
@@ -1,11 +1,11 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
-import { pedirDatos } from '../../helpers/pedirDatos'
+import { db } from '../../firebase/config'
 import { ItemList } from '../ItemList/ItemList'
 import { Loader } from '../Loader/Loader'
+import { collection, getDocs, query, where, limit } from 'firebase/firestore/lite'
 
 export const ItemListContainer = () => {
-
     
     const [items, setItems] = useState([])
     const [loading, setLoading] = useState(false)
@@ -15,21 +15,27 @@ export const ItemListContainer = () => {
     useEffect(()=>{
 
         setLoading(true)
-        pedirDatos()
-            .then((resp) => {
-                if (categoryId) {
-                    setItems( resp.filter((el) => el.cat === categoryId) )
-                } else {
-                    setItems( resp )
-                }
 
-            })
-            .catch( (err) => {
-                console.log(err)
+        // crear la referencia a la colección
+        const productosRef = collection(db, 'productos')
+        const q = categoryId ? query(productosRef, where('cat', '==', categoryId)) : productosRef
+        // llamamos a esa ref
+        getDocs(q)
+            .then( resp => {
+                const productos = resp.docs.map( (doc) => {
+                    return {
+                        id: doc.id,
+                        ...doc.data()
+                    }
+                })
+                console.log(productos)
+
+                setItems(productos)
             })
             .finally(() => {
                 setLoading(false)
             })
+       
 
     }, [categoryId])
 
